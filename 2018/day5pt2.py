4753314ab1e0a4e95729b20859e28ba7b347a65a
larry <lmoiola@gmail.com>
2020-01-19 13:11:02 -0800

    clean up comments

--- 2018/day5pt2.py
+++ 2018/day5pt2.py
@@ -18,11 +18,11 @@ In this example, removing all C/c units was best, producing the answer 4.
 
 What is the length of the shortest polymer you can produce by removing all units of exactly one type and fully reacting the result?
 '''
-from day5pt1 import reducePolymer # imports main function from Day 5, part 1 (implies use of removePairs())
-import string                     # import for input data
+from day5pt1 import reducePolymer           # imports main function from Day 5, part 1 (implies use of removePairs())
+import string                               # import for input data
 import time
 
-start_time = time.time()
+start_time = time.time()                    # start the timer (see line 43)
 
 letters = string.ascii_lowercase                # generate data for input: a-z
 results = {}                                    # initialize temp list
